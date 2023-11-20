7f0c93f3824fb904f22c84dc1b180b0c1afba788
DESKTOP-T6VKH5T\pradeep <Pradeep7manasa>
2023-11-20 19:37:06 +0530

    update of repository(MVN)

---
+++ src/test/java/test/w3school.java
@@ -0,0 +1,10 @@
+package test;
+
+public class w3school {
+    String lllllllll = "Pradeep";
+    public static void main(String[] args) {
+        w3school test = new w3school();
+        System.out.println(test.lllllllll);
+    }
+    
+}
